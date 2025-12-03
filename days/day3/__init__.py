ff35cba7f236f7e027df1e0082c64919bd35ecd7
Lucas Alenbro Lindström <lucas.lindstrom@vermiculus.se>
2025-12-03 09:10:13 +0100

    Day 3

---
+++ days/day3/__init__.py
@@ -0,0 +1,50 @@
+def joltage(battery: str, size: int) -> int:
+    """Calculates the joltage of the battery string.
+
+    >>> joltage('811111111111119', 2)
+    89
+    >>> joltage('987654321111111', 2)
+    98
+    >>> joltage('234234234234278', 2)
+    78
+    >>> joltage('818181911112111', 2)
+    92
+    >>> joltage('987654321111111', 12)
+    987654321111
+    >>> joltage('811111111111119', 12)
+    811111111119
+    """
+
+    idx = 0
+    value = ""
+    for i in range(size):
+        start = idx
+        end = i + -size + 1
+        if end == 0:
+            end = None
+
+        mx = max(battery[start:end])
+        idx = battery.index(mx, start, end) + 1
+        value += mx
+
+    return int(value)
+
+
+def star1(input_str: str) -> str:
+    """
+    >>> star1('811111111111119\\n987654321111111\\n234234234234278\\n818181911112111')
+    '357'
+    """
+    batteries = input_str.strip().split("\n")
+    total_joltage = sum(joltage(battery, 2) for battery in batteries)
+    return str(total_joltage)
+
+
+def star2(input_str: str) -> str:
+    """
+    >>> star2('811111111111119\\n987654321111111\\n234234234234278\\n818181911112111')
+    '3121910778619'
+    """
+    batteries = input_str.strip().split("\n")
+    total_joltage = sum(joltage(battery, 12) for battery in batteries)
+    return str(total_joltage)
