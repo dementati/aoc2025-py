66e3605faf29810ba485bc3852becb79b4566a4f
Lucas Alenbro Lindström <lucas.lindstrom@vermiculus.se>
2025-12-05 08:21:27 +0100

    Line sweep for d5p1

--- days/day5/__init__.py
+++ days/day5/__init__.py
@@ -48,20 +48,41 @@ def read_example() -> tuple[list[Range], list[int]]:
     return parse_input(input_str)
 
 
+def read_input() -> tuple[list[Range], list[int]]:
+    with open("days/day5/input.txt") as file:
+        input_str = file.read()
+    return parse_input(input_str)
+
+
 def fresh(ranges: list[Range], numbers: list[int]) -> int:
     """
     >>> fresh(*read_example())
     3
+    >>> fresh(*read_input())
+    598
     """
     merged = merge_all(ranges)
 
-    return sum(1 for number in numbers if any(r.contains(number) for r in merged))
+    sorted_numbers = sorted(numbers)
+    i = 0
+    count = 0
+    for n in sorted_numbers:
+        while i < len(merged) and merged[i].end < n:
+            i += 1
+        if i == len(merged):
+            break
+        if merged[i].contains(n):
+            count += 1
+
+    return count
 
 
 def merge_all(ranges: list[Range]) -> list[Range]:
     """
     >>> count_ranges(merge_all(read_example()[0]))
     14
+    >>> count_ranges(merge_all(read_input()[0]))
+    360341832208407
     """
     sorted_ranges = sorted(ranges, key=lambda r: (r.start, r.end))
     merged = [sorted_ranges[0]]
